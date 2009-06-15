d442bbc9ab120be30458da0a1be04c942794d930
Matthew Hollingworth <mdholling@gmail.com>
2009-06-16 08:28:42 +1000

    Added spec for User.first.articles.first.next being nil when it shouldn't. Fixed find_with_context to pass spec.

--- lib/paged_scopes/context.rb
+++ lib/paged_scopes/context.rb
@@ -11,11 +11,11 @@ module PagedScopes
     
     module ClassMethods
       def find_with_context(*args)
+        found_scope, found_options = scope(:find) || {}, args.dup.extract_options!.dup
         returning find_without_context(*args) do |results|
-          found_scope, found_options = scope(:find), args.extract_options!
           [ results ].flatten.compact.each do |result|
-            result.instance_variable_set "@found_scope", found_scope
-            result.instance_variable_set "@found_options", found_options
+            result.instance_variable_set "@found_scope", found_scope.dup
+            result.instance_variable_set "@found_options", found_options.dup
           end
         end
       end
--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -15,5 +15,28 @@ describe "Context" do
         articles.pop.previous.should == articles.last
       end
     end
-  end  
+    
+    it "should be know for an object obtained with #first" do
+      @articles.all.should have_at_least(2).articles
+      @articles.first.next.should_not be_nil
+      @articles.first.previous.should be_nil
+    end
+
+    it "should be known for object for an object obtained with #last" do
+      @articles.all.should have_at_least(2).articles
+      @articles.last.previous.should_not be_nil
+      @articles.last.next.should be_nil
+    end
+  end
+
+  context "for an object found with #first" do
+    before(:each) do
+      @articles = User.first.articles
+    end
+
+    it "should know its next object" do
+      @articles.all.should have_at_least(2).items
+      @articles.first.next.should_not be_nil
+    end
+  end
 end
