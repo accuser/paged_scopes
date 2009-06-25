c49d5337d6bf18e3a652a79252278504ac21ef0d
Matthew Hollingworth <mdholling@gmail.com>
2009-06-25 15:47:02 +1000

    Changed paginator so gap and select information is passes as an options hash to the yield.

--- lib/paged_scopes/paginator.rb
+++ lib/paged_scopes/paginator.rb
@@ -22,43 +22,29 @@ module PagedScopes
       path.call(@page.next) unless @page.last?
     end
 
-    # def window(options)
-    #   raise ArgumentError, "please specify a :inner option" unless size = options[:inner]
-    #   :outer
-    #   extras = [ options[:extras] ].flatten.compact
-    #   raise ArgumentError, "No window block supplied." unless block_given?
-    #   returning [] do |results|
-    #     results << yield(:first, @page.first? ? nil : @path.call(@page.class.first)) if extras.include?(:first)
-    #     results << yield(:previous, @page.first? ? nil : @path.call(@page.previous)) if extras.include?(:previous)
-    #     (-size..size).map { |offset| @page.offset(offset) }.compact.each do |page|
-    #       results << yield(page, @path.call(page))
-    #     end
-    #     results << yield(:next, @page.last? ? nil : @path.call(@page.next)) if extras.include?(:next)
-    #     results << yield(:last, @page.last? ? nil : @path.call(@page.class.last)) if extras.include?(:last)
-    #   end.join("\n")
-    # end
-
     def window(options)
       # return if one page...?
+      raise ArgumentError, "No window block supplied." unless block_given?
       raise ArgumentError, "please specify a :inner option" unless inner = options[:inner]
       outer = options[:outer] || 0
       extras = [ options[:extras] ].flatten.compact
-      raise ArgumentError, "No window block supplied." unless block_given?
+      numbers = (@page.number-inner..@page.number+inner).to_a
+      1.upto(outer) { |n| numbers << n << @page.page_count-n+1 }
+      numbers = numbers.uniq.sort.select { |n| n.between?(1, @page.page_count) }
       returning [] do |results|
-        results << yield(:first, @page.first? ? nil : @path.call(@page.class.first)) if extras.include?(:first)
-        results << yield(:previous, @page.first? ? nil : @path.call(@page.previous)) if extras.include?(:previous)
-        numbers = (@page.number-inner..@page.number+inner).to_a
-        outer.times { |n| numbers.unshift(n+1).push(@page.page_count-n) }
-        numbers.uniq.sort.select do |number|
-          number.between?(1, @page.page_count)
-        end.inject(nil) do |previous, number|
-          results << yield(:separator, nil) if previous && previous < number - 1
+        results << yield(:first, @page.first? ? nil : @path.call(@page.class.first), {}) if extras.include?(:first)
+        results << yield(:previous, @page.first? ? nil : @path.call(@page.previous), {}) if extras.include?(:previous)
+        numbers.zip([nil]+numbers, numbers[1..-1]) do |number, prev_number, next_number|
           page = @page.class.find(number)
-          results << yield(page, page == @page ? nil : @path.call(page))
-          number
+          path = page == @page ? nil : @path.call(page)
+          opts = {}
+          opts[:selected] = page == @page
+          opts[:gap_before] = prev_number ? prev_number < number - 1 : false
+          opts[:gap_after] = next_number ? next_number > number + 1 : false
+          results << yield(page, path, opts)
         end        
-        results << yield(:next, @page.last? ? nil : @path.call(@page.next)) if extras.include?(:next)
-        results << yield(:last, @page.last? ? nil : @path.call(@page.class.last)) if extras.include?(:last)
+        results << yield(:next, @page.last? ? nil : @path.call(@page.next), {}) if extras.include?(:next)
+        results << yield(:last, @page.last? ? nil : @path.call(@page.class.last), {}) if extras.include?(:last)
       end.join("\n")
     end
   end
