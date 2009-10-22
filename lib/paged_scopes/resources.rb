c3ed9c46fec1653dd174b1d9cf0f29638dd29b9e
Matthew Gibbons <mhgibbons@me.com>
2009-10-22 02:46:00 +0100

    Added ability to generate paged routes for resource collection methods

--- lib/paged_scopes/resources.rb
+++ lib/paged_scopes/resources.rb
@@ -2,16 +2,41 @@ module PagedScopes
   module Resources
     def resources_with_paged(*entities, &block)
       options = entities.extract_options!
-      if page_options = options.delete(:paged)
-        resources_without_paged(*(entities.dup << options), &block)
-        page_options = {} unless page_options.is_a? Hash
-        page_name = page_options.delete(:name)
-        page_options.slice!(:as, :name)
-        page_options.merge!(:only => :none)
-        preserved_options = ActionController::Resources::INHERITABLE_OPTIONS + [ :name_prefix, :path_prefix ]
-        with_options(options.slice(*preserved_options)) do |map|
-          map.resources_without_paged(page_name || :pages, page_options) do |page|
-            page.resources(*(entities.dup << { :only => :index }))
+
+      if paged_options = options.delete(:paged)
+        resources_without_paged(*(entities.dup << options), &block)        
+        
+        paged_options = {} unless paged_options.is_a? Hash
+        
+        paged_as = paged_options.delete(:as)
+        paged_name = paged_options.delete(:name)
+        
+        if paged_options.empty?
+          if (options[:only] && options[:only].include?(:index)) || !(options[:except] && options[:except].include?(:index))
+            paged_options.merge! :index => true
+          end
+          
+          options[:collection].each_pair { |k,v| paged_options[k] = true if v == :get } if options[:collection].is_a? Hash
+        end
+
+        paged_options.each_pair do |action,page_options|
+          page_options = {} unless page_options.is_a? Hash
+          
+          page_options.reverse_merge! :name => paged_name unless paged_name.blank?
+          page_options.reverse_merge! :as => paged_as unless paged_as.blank?
+
+          page_options[:only] = []
+          
+          preserved_options = ActionController::Resources::INHERITABLE_OPTIONS + [ :name_prefix, :path_prefix ]
+          
+          with_options(options.slice(*preserved_options)) do |map|
+            map.resources_without_paged(page_options.delete(:name) || :pages, page_options) do |page|
+              if action == :index
+                page.resources(*(entities.dup << { :only => :index, :as => options[:as] }))
+              else
+                page.resources(*(entities.dup << { :only => [], :as => options[:as], :collection => { action => :get }}))
+              end
+            end
           end
         end
       else
